cac5061864ec1b4c1cfc75da985e0d18c714fc20
nassafou <ntyoussouf@gmail.com>
2020-03-11 14:58:20 +0000

    callback

---
+++ callback_plugins/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Make coding more python3-inh
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.plugins.callback.default import CallbackModule as CallbackModule_default
+import json
+# This callback inherit from default callback
+class CallbackModule(CallbackModule_default):
+    CALLBACK_VERSION = 2.0
+    CALLBACK_NAME = 'test'
+#    def _dump_results(self, result, indent=None, sort_keys=True, keep_invocation=False):
+#        return CallbackModule_default._dump_results(self, result, indent=4, sort_keys=True, keep_invocation=False)
